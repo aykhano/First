ddb19e861968f2f8fd061934690a9faff9659fed
Ayxan <aykhanoscar556@gmail.com>
2025-02-08 14:29:08 +0400

    todat status

--- blogapp/account/views.py
+++ blogapp/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def login_request(request):
     if request.method == "POST":
@@ -19,6 +20,54 @@ def login_request(request):
     return render(request, "account/login.html")
 
 def register_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request, "account/register.html", 
+                {
+                    "error":"username is exist", 
+                    "username":username,
+                    "email":email,
+                    "firstname":firstname,
+                    "lastname":lastname,
+                    "username":username,
+                })
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, "account/register.html", 
+                    {
+                        "error":"email is exist", 
+                        "username":username,
+                        "email":email,
+                        "firstname":firstname,
+                        "lastname":lastname,
+                        "username":username,
+                    })
+                else:
+                    user = User.objects.create_user(username=username, email=email, first_name=firstname, 
+                    last_name=lastname, password=password)
+                    user.save()
+                    return redirect("login")
+        else:
+            return render(request, "account/register.html", 
+            {
+                "error":"password is wrong!",
+                "error":"email is exist", 
+                "username":username,
+                "email":email,
+                "firstname":firstname,
+                "lastname":lastname,
+                "username":username,
+
+            })
+
     return render(request, "account/register.html")
 
 def logout_request(request):
